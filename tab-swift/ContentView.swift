c99465eb7823cc312af75b7f2ea52afd524cba29
novenkov <ynovenkov@yandex.ru>
2020-05-04 23:56:23 +0300

    Initial Commit

---
+++ tab-swift/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  tab-swift
+//
+//  Created by yn on 04.05.2020.
+//  Copyright © 2020 yn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
